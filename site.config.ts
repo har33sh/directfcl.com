3e94ffff680626943fb71babb39d21417109697d
Hareesh Kumar <hareesh@live.in>
2023-12-09 14:44:13 +0530

    not adding space id

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'f1d335a7f64d4d9fad36b4da200cca38',
+//   rootNotionSpaceId: 'f1d335a7f64d4d9fad36b4da200cca38',
 
   // basic site info (required)
   name: 'Transitive Bullshit',
