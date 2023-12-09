7f4db3a9c053e336247dd1ebd6e24e34734ce1c7
Hareesh Kumar <hareesh@live.in>
2023-12-09 12:40:40 +0530

    deploy

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'directFCL-f50ef08e0b2d4cf29607830f8461cd87',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
